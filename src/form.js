ff26873f26efa811cf5a2928a34b66c4bd91b2cf
fyy <fyy@MacBook-Pro-fyy.local>
2022-08-03 20:33:07 +0300

    add: envybox

--- src/form.js
+++ src/form.js
@@ -14,45 +14,63 @@ elements.forEach(e => {
 /*
 * Обработка формы и отправка в Битрикс24
 */
-const bitrix_api = 'https://avirent.bitrix24.ru/rest/3213/nwob5jfa6b5yeqn2/'
-const bitrix_method = 'crm.lead.add.json'
+const link = 'https://okbmetallceh.envycrm.com/crm/api/v1/lead/workset/?api_key=e3933159a9027843400f789236a6997abb608dff';
 
 const forms = document.querySelectorAll('form')
 forms.forEach(el => {
   el.addEventListener('submit', async (e) => {
     e.preventDefault();
 
-    const input = e.target.elements[0];
-    const btn = e.target.elements[1];
+    const input = e.target.querySelector('input');
+    if (input.value.length < 16) {
+      alert('Введите корректный номер')
+      return false
+    } 
 
     const dataForm = {
-      fields:
-      {
-        "TITLE": "Новая заявка с сайта",
-        "OPENED": "Y",
-        "ASSIGNED_BY_ID": 3589,
-        "SOURCE_DESCRIPTION": `Заполненная форма: ${btn.textContent == 'Скачать прайс-лист' ? input.placeholder + '. Отправить прайс' : btn.textContent}`,
-        "PHONE": [{ "VALUE": input.value, "VALUE_TYPE": "WORK" }]
-      },
-      params: { "REGISTER_SONET_EVENT": "Y" }
+      request: {
+        method: "create",
+        values: {
+          "phone": input.value,
+          "comment": "Заявка с сайта лид-магнита"
+        }
+      }
     }
 
+    await sendToTelegram(input)
     sendForm(dataForm)
       .then((data) => {
         console.log(data);
         window.location.href = "https://lestnicamy.ru/thanks-katalog/";
       })
       .catch(er => alert('Ошибка отправки данных\n' + er))
+    
   })
 })
 
 async function sendForm(data) {
-  const res = await fetch(bitrix_api + bitrix_method, {
+  const res = await fetch(link, {
     method: 'POST',
+    mode: 'no-cors',
     headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(data)
   })
-  return await res.json()
+  return await res
+}
+
+async function sendToTelegram(tel) {
+  const token = "5594300094:AAEHLSNb-mGIWJ8t5Zz-MRJw9MsMZrB7yEU";  
+  const telegramUrl = "https://api.telegram.org/bot" + token;  
+  const id = "-1001746821085";
+  const text = "<u> НОВАЯ ЗАЯВКА</u> \n" + 
+            "Телефон: " + "<b>" + tel.value + "</b> \n" + 
+            "Источник заявки: " + "<b>" + "сайт Лид-магнит" + "</b> \n";  
+  const url = telegramUrl + "/sendMessage?chat_id=" + id + "&parse_mode=html&text=" + text;
+
+  const res = await fetch(encodeURI(url), {
+    method: 'GET',
+  })
+  return await res
 }
